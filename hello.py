aee685baf58a8e0d145740df9fd5e642eab03f47
kondo takeshi <chaspy@users.noreply.github.com>
2021-09-18 11:57:34 +0900

    Add hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from bcc import BPF
+from bcc.utils import printb
+
+# define BPF program
+bpf_text= """
+int hello(void *ctx) {
+    bpf_trace_printk("Hello, World!\\n");
+        return 0;
+    }
+"""
+
+# load BPF program
+b = BPF(text=bpf_text)
+b.attach_kprobe(event=b.get_syscall_fnname("clone"), fn_name="hello")
+b.trace_print()
